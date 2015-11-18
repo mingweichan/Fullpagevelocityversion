27a9f4c4d5fa8bcdc65cc4b803b83fd6e78d9438
Ming wei Chan <mcha1524@uni.sydney.edu.au>
2015-11-18 23:58:31 +1100

    changed a lot
    finished carousel, up to how does it work

--- js/main.js
+++ js/main.js
@@ -22,19 +22,19 @@ $(document).ready(function(){
     slidesNavigation: true,
     css3: true,
     controlArrows: false,
-    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection', 'seventhSection', 'eighthSection'],
     menu: '#menu',
 
     afterLoad: function(anchorLink, index) {
       $header_top.css('background', 'rgba(0, 47, 77, .3)');
       $nav.css('background', 'rgba(0, 47, 77, .25)');
-      if (index == 5) {
+      if (index == 2) {
           $('#fp-nav').hide();
         }
     },
 
     onLeave: function(index, nextIndex, direction) {
-      if(index == 5) {
+      if(index == 2) {
         $('#fp-nav').show();
       }
     },
