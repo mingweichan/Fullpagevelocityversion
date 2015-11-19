42d01b74735ac2a0583ef59bb4bbf812928a5f2c
Ming wei Chan <mcha1524@uni.sydney.edu.au>
2015-11-19 18:13:23 +1100

    lastcommit

--- js/main.js
+++ js/main.js
@@ -1,79 +1,79 @@
+$(document).ready(function () {
 
-$(document).ready(function(){
-    
-    $(".wtf").click(function(){
-        $(".tandc-form").addClass('displaynone',{duration:1000});     
-});
-    $(".wtf").click(function(){
-        $(".thanks").addClass('displayfun',{duration:1000});     
-});
-    
-  // variables
-  var $header_top = $('.header-top');
-  var $nav = $('nav');
+    $(".wtf").click(function () {
+        $(".tandc-form").addClass('displaynone', {
+            duration: 1000
+        });
+    });
+    $(".wtf").click(function () {
+        $(".thanks").addClass('displayfun', {
+            duration: 1000
+        });
+    });
 
-
-
-  // toggle menu 
-  $header_top.find('a').on('click', function() {
-    $(this).parent().toggleClass('open-menu');
-  });
+    // variables
+    var $header_top = $('.header-top');
+    var $nav = $('nav');
 
 
 
-  // fullpage customization
-  $('#fullpage').fullpage({
-    sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
-    sectionSelector: '.vertical-scrolling',
-    slideSelector: '.horizontal-scrolling',
-    navigation: true,
-    slidesNavigation: true,
-    css3: true,
-    controlArrows: false,
-    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection','sixthSection', 'seventhSection', 'eighthSection'],
-    menu: '#menu',
+    // toggle menu 
+    $header_top.find('a').on('click', function () {
+        $(this).parent().toggleClass('open-menu');
+    });
 
-    afterLoad: function(anchorLink, index) {
-      $header_top.css('background', 'rgba(0, 47, 77, .3)');
-      $nav.css('background', 'rgba(0, 47, 77, .25)');
-      if (index == 2) {
-          $('#fp-nav').hide();
+
+
+    // fullpage customization
+    $('#fullpage').fullpage({
+        sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+        sectionSelector: '.vertical-scrolling',
+        slideSelector: '.horizontal-scrolling',
+        navigation: true,
+        slidesNavigation: true,
+        css3: true,
+        controlArrows: false,
+        anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection', 'seventhSection', 'eighthSection'],
+        menu: '#menu',
+
+        afterLoad: function (anchorLink, index) {
+            $header_top.css('background', 'rgba(0, 47, 77, .3)');
+            $nav.css('background', 'rgba(0, 47, 77, .25)');
+            if (index == 2) {
+                $('#fp-nav').hide();
+            }
+        },
+
+        onLeave: function (index, nextIndex, direction) {
+            if (index == 2) {
+                $('#fp-nav').show();
+            }
+        },
+
+        afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+            if (anchorLink == 'fifthSection' && slideIndex == 1) {
+                $.fn.fullpage.setAllowScrolling(false, 'up');
+                $header_top.css('background', 'transparent');
+                $nav.css('background', 'transparent');
+                $(this).css('background', '#374140');
+                $(this).find('h2').css('color', 'white');
+                $(this).find('h3').css('color', 'white');
+                $(this).find('p').css({
+                    'color': '#DC3522',
+                    'opacity': 1,
+                    'transform': 'translateY(0)'
+                });
+            }
+        },
+
+        onSlideLeave: function (anchorLink, index, slideIndex, direction) {
+            if (anchorLink == 'fifthSection' && slideIndex == 1) {
+                $.fn.fullpage.setAllowScrolling(true, 'up');
+                $header_top.css('background', 'rgba(0, 47, 77, .3)');
+                $nav.css('background', 'rgba(0, 47, 77, .25)');
+            }
         }
-    },
-
-    onLeave: function(index, nextIndex, direction) {
-      if(index == 2) {
-        $('#fp-nav').show();
-      }
-    },
-
-    afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
-      if(anchorLink == 'fifthSection' && slideIndex == 1) {
-        $.fn.fullpage.setAllowScrolling(false, 'up');
-        $header_top.css('background', 'transparent');
-        $nav.css('background', 'transparent');
-        $(this).css('background', '#374140');
-        $(this).find('h2').css('color', 'white');
-        $(this).find('h3').css('color', 'white');
-        $(this).find('p').css(
-          {
-            'color': '#DC3522',
-            'opacity': 1,
-            'transform': 'translateY(0)'
-          }
-        );
-      }
-    },
-
-    onSlideLeave: function( anchorLink, index, slideIndex, direction) {
-      if(anchorLink == 'fifthSection' && slideIndex == 1) {
-        $.fn.fullpage.setAllowScrolling(true, 'up');
-        $header_top.css('background', 'rgba(0, 47, 77, .3)');
-        $nav.css('background', 'rgba(0, 47, 77, .25)');
-      }
-    } 
-  }); 
+    });
 
 
 });
-
